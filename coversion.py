eba9a02e7ffc3ef4c185873818488c99a319389a
Muskan-2504 <muskanmkg25@gmail.com>
2025-09-27 22:15:54 +1000

    method changed

--- coversion.py
+++ coversion.py
@@ -1,6 +1,1 @@
-import pandas as pd
-
-# Load your Excel files
-un_df = pd.read_excel("undesa_pd_2015_migration_flow_totals.xlsx")
-print("Columns:", un_df.columns.tolist())
-print(un_df.head())
+df = pd.read_csv("https://github.com/Muskan0019monash/3179-data-visualization/blob/dev/undesa_pd_2015_migration_flow_totals.xlsx")
